cbf96263eeb117e69146d3bc1b2c6ed8f26bee5f
Raijin2212 <fliance2006@gmail.com>
2026-05-25 20:35:50 +0800

    dessert spelling checked

--- Main/Dessert.java
+++ Main/Dessert.java
@@ -1,6 +1,6 @@
 public class Dessert extends MenuItem {
 
     public Dessert(int id, String itemName, double price, int stockQuantity) {
-        super(id, itemName, price, stockQuantity, "Desert");
+        super(id, itemName, price, stockQuantity, "Dessert");
     }
 }
